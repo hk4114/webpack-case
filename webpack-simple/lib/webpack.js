b6f1a2e782c03f29884c6d0d58ea0f07abbed98f
kz-fe <741180788@qq.com>
2021-09-03 20:27:13 +0800

    update: 读取入口文件内容

--- webpack-simple/lib/webpack.js
+++ webpack-simple/lib/webpack.js
@@ -1,8 +1,14 @@
+const fs = require('fs')
+
 module.exports = class webpack {
   constructor(options) {
-    console.log(options, 'sign');// 打印 webpack 参数
+    const { entry, output } = options;
+    this.entry = entry;
+    this.output = output;
   }
   run() {
-
+    // 分析入口模块的依赖和内容
+    const content = fs.readFileSync(this.entry, 'utf-8');
+    console.log(content, 'sign')
   }
 }
